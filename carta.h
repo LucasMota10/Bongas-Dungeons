86e01837e4219b23e33ee609a889d03735e74fcf
LucasMota10 <lucasbulcaobmota@gmail.com>
2023-09-28 23:12:44 -0300

    modified:   arte.h 	new file:   caminho.h 	modified:   carta.h 	modified:   menu.h 	modified:   monstro.h 	modified:   player.h

--- carta.h
+++ carta.h
@@ -1,102 +1,167 @@
 #ifndef carta_h
 #define carta_h
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdlib.h>
-#include "pilha.h"
 
-//tipo de carta por num (0==ataque , 1==defesa , especial == 2)
+#define MAX 100
 
-//cartas do deck
-void criar_cartas(tp_pilha *pilha){
+typedef struct {
+  char nome[30];
+  int tipodacarta; // tipo de carta por num (0==ataque , 1==defesa , especial ==
+                   // 2)
+  int power, mana, regmana, vida, escudo, level;
+} tp_cartas;
 
-tp_cartas cartas[12];
+// LIB DE PILHA
 
-//cartas de ataque
+typedef struct {
+  tp_cartas item[MAX];
+  int topo;
+} tp_pilha;
 
-strcpy(cartas[0].nome,"Bola de Fogo");
-cartas[0].tipodacarta = 0;
-cartas[0].power = 10;
-cartas[0].mana = 20;
+void inicializa_pilha(tp_pilha *p) { p->topo = -1; }
 
-strcpy(cartas[1].nome,"Soco");
-cartas[1].tipodacarta = 0;
-cartas[1].power = 5;
-cartas[1].mana = 5;
+int pilha_vazia(tp_pilha *p) {
+  if (p->topo == -1)
+    return 1;
+  return 0;
+}
 
-strcpy(cartas[2].nome,"Flecha da tormenta");
-cartas[2].tipodacarta = 0;
-cartas[2].power = 15;
-cartas[2].mana = 10;
+int pilha_cheia(tp_pilha *p) {
+  if (p->topo == MAX - 1)
+    return 1;
+  return 0;
+}
 
-strcpy(cartas[3].nome,"Sistemas Digitais");
-cartas[3].tipodacarta = 0;
-cartas[3].power = 25;
-cartas[3].mana = 30;
+int push(tp_pilha *p, tp_cartas f) {
+  if (pilha_cheia(p))
+    return 0;
+  p->topo++;
+  p->item[p->topo] = f;
+  return 1;
+}
+int pop(tp_pilha *p, tp_cartas *e) {
+  if (pilha_vazia(p))
+    return 0;
+  *e = p->item[p->topo];
+  p->topo--;
+  return 1;
+}
 
-//cartas de defesa
+int top(tp_pilha *p, tp_cartas *e) {
+  if (pilha_vazia(p))
+    return 0;
+  *e = p->item[p->topo];
+  return 1;
+}
 
-strcpy(cartas[4].nome,"Camisa Laranja");
-cartas[4].tipodacarta = 1;
-cartas[4].mana = 10;
-cartas[4].escudo = 20;
+int altura_pilha(tp_pilha *p) { return p->topo + 1; }
 
-strcpy(cartas[5].nome,"Lando Lando");
-cartas[5].tipodacarta = 1;
-cartas[5].mana = 15;
-cartas[5].escudo = 30;
+// cartas do deck
 
-strcpy(cartas[6].nome,"Obrigado Soussa");
-cartas[6].tipodacarta = 1;
-cartas[5].mana = 5;
-cartas[5].escudo = 20;
+void criar_cartas(tp_pilha *pilha) {
 
-strcpy(cartas[7].nome,"Pato");
-cartas[7].tipodacarta = 1;
-cartas[7].mana = 50;
-cartas[7].escudo = 30;
+  tp_cartas cartas[12];
 
-// cartas de especial
+  // cartas de ataque
 
-strcpy(cartas[8].nome,"Lanche no garfo de ouro");
-cartas[8].tipodacarta = 2;
-cartas[8].vida = 20;
-cartas[8].mana = 10;
+  strcpy(cartas[0].nome, "Bola de Fogo");
+  cartas[0].tipodacarta = 0;
+  cartas[0].power = 10;
+  cartas[0].mana = 20;
 
-strcpy(cartas[9].nome,"Neneca no cyber");
-cartas[9].tipodacarta = 2;
-cartas[9].vida = 50;
-cartas[9].mana = 30;
+  strcpy(cartas[1].nome, "Soco");
+  cartas[1].tipodacarta = 0;
+  cartas[1].power = 5;
+  cartas[1].mana = 5;
 
-strcpy(cartas[10].nome,"Masterizou AED");
-cartas[10].tipodacarta = 2;
-cartas[10].mana = 0;
-cartas[10].regmana = 60;
+  strcpy(cartas[2].nome, "Flecha da tormenta");
+  cartas[2].tipodacarta = 0;
+  cartas[2].power = 15;
+  cartas[2].mana = 10;
 
-strcpy(cartas[11].nome,"Sabedoria suprema");
-cartas[11].tipodacarta = 2;
-cartas[11].power = 10;
-cartas[11].mana = 10;
-cartas[11].vida = 10;
-cartas[11].escudo = 10;
+  strcpy(cartas[3].nome, "Sistemas Digitais");
+  cartas[3].tipodacarta = 0;
+  cartas[3].power = 25;
+  cartas[3].mana = 30;
 
-//Atribuindo os niveis das cartas
-int lvl = 1;
-for(int i = 0 ; i<12 ; i++){
+  // cartas de defesa
+
+  strcpy(cartas[4].nome, "Camisa Laranja");
+  cartas[4].tipodacarta = 1;
+  cartas[4].mana = 10;
+  cartas[4].escudo = 20;
+
+  strcpy(cartas[5].nome, "Lando Lando");
+  cartas[5].tipodacarta = 1;
+  cartas[5].mana = 15;
+  cartas[5].escudo = 30;
+
+  strcpy(cartas[6].nome, "Obrigado Soussa");
+  cartas[6].tipodacarta = 1;
+  cartas[5].mana = 5;
+  cartas[5].escudo = 20;
+
+  strcpy(cartas[7].nome, "Pato");
+  cartas[7].tipodacarta = 1;
+  cartas[7].mana = 50;
+  cartas[7].escudo = 30;
+
+  // cartas de especial
+
+  strcpy(cartas[8].nome, "Lanche no garfo de ouro");
+  cartas[8].tipodacarta = 2;
+  cartas[8].vida = 20;
+  cartas[8].mana = 10;
+
+  strcpy(cartas[9].nome, "Neneca no cyber");
+  cartas[9].tipodacarta = 2;
+  cartas[9].vida = 50;
+  cartas[9].mana = 30;
+
+  strcpy(cartas[10].nome, "Masterizou AED");
+  cartas[10].tipodacarta = 2;
+  cartas[10].mana = 0;
+  cartas[10].regmana = 60;
+
+  strcpy(cartas[11].nome, "Sabedoria suprema");
+  cartas[11].tipodacarta = 2;
+  cartas[11].power = 10;
+  cartas[11].mana = 10;
+  cartas[11].vida = 10;
+  cartas[11].escudo = 10;
+
+  // Atribuindo os niveis das cartas
+  int lvl = 1;
+  int i;
+  for (i = 0; i < 12; i++) {
     cartas[i].level = lvl;
     lvl++;
-    if(lvl>4) lvl = 1;
-}
+    if (lvl > 4)
+      lvl = 1;
+  }
 
-//Função preencher pilha:
-srand(time(NULL));
-for (int i = 0; i < 12; i++) {    // shuffle array
-    
-    int random = rand()%11;
+  // Função preencher pilha:
+  srand(time(NULL));
+  for (i = 0; i < 10; i++) { // shuffle array
+
+    int random = rand() % 11;
     push(pilha, cartas[random]);
-
+  }
 }
+
+void imprime_cartas(tp_cartas *p) {
+
+  printf("============================================\n");
+  printf("Cartas do jogador: \n");
+
+  for (int i = 0; i < 5; i++) {
+
+    printf("%d.: %s\n", i + 1, p[i].nome);
+  }
+  printf("============================================\n\n");
 }
 
 #endif
--- player.h
+++ player.h
@@ -1,72 +1,81 @@
 #ifndef player_h
 #define player_h
+#include "carta.h"
 #include <stdio.h>
-#include "pilha.h"
 
-typedef struct{
-char nome[50];
-int vida, mana, escudo;
+typedef struct {
+  char nome[50];
+  int vida, mana, escudo, level; // 200 de vida, 50 de mana, 0 de escudo
 
-}tp_player;
+} tp_player;
 
-void sacar_deck(tp_pilha *cartas_baralho, tp_pilha *cartas_jogador){
+void criarplayer(tp_player *jogador) {
+  jogador->vida = 200;
+  jogador->mana = 50;
+  jogador->escudo = 0;
+  jogador->level = 1;
+  
+}
 
-tp_cartas deck;
-for(int i = 0; i<5; i++){
+void sacar_deck(tp_pilha *cartas_baralho, tp_cartas *cartas_jogador) {
+
+  tp_cartas deck;
+
+  for (int i = 0; i < 5; i++) {
 
     pop(cartas_baralho, &deck);
-    push(cartas_jogador, deck);
-}
-}
-
-void imprime_deck(tp_pilha cartas_jogador){
-imprime_pilha(cartas_jogador);
+    cartas_jogador[i] = deck;
+  }
 }
 
-void imprime_player(tp_player jogador, tp_pilha pilha){
-printf("Nome: %s\n", jogador.nome);
-printf("Vida: %d/100 \n", jogador.vida);
-printf("Mana: %d/50 \n", jogador.mana);
-printf("Escudo: %d/50 \n", jogador.escudo);
-imprime_pilha(pilha);
+void imprime_player(tp_player jogador, tp_cartas *carta) {
+  printf("============================================\n");
+  printf("Nome: %s\n", jogador.nome);
+  printf("Vida: %d/200 \n", jogador.vida);
+  printf("Mana: %d/50 \n", jogador.mana);
+  printf("Escudo: %d/50 \n", jogador.escudo);
+  printf("============================================\n\n");
+
+  imprime_cartas(carta);
 }
 
-int checar_custo(tp_player *jogador, tp_pilha *e){
+int checar_custo(tp_player *jogador, tp_pilha *e) {
 
-tp_cartas j;
+  tp_cartas j;
 
-top(e, &j);
-if(j.mana > jogador->mana) return 0;
-return 1;
+  top(e, &j);
+  if (j.mana > jogador->mana)
+    return 0;
+  return 1;
 }
 
-void usar_cartas(tp_pilha *cartas_jogador, tp_player *jogador){
-tp_cartas e;
+void usar_carta(tp_pilha *cartas_jogador, tp_player *jogador) {
+  tp_cartas e;
 
-if(!checar_custo(jogador, cartas_jogador)){
-     printf("Mana insuficiente\n");
-}
-else{
+  if (!checar_custo(jogador, cartas_jogador)) {
+    printf("Mana insuficiente\n");
+  } else {
 
-pop(cartas_jogador, &e);
-jogador->mana = jogador->mana - e.mana;
-switch (e.tipodacarta)
-{
-case 0:
-    /* code */
-    break;
+    if (pilha_vazia(cartas_jogador)) {
+    }
+    pop(cartas_jogador, &e);
+    jogador->mana = jogador->mana - e.mana;
+    switch (e.tipodacarta) {
+    case 0:
+      // monstro->vida -= e.power;
+      break;
 
-case 1:
-    /* code */
-    break;
+    case 1:
+      /* code */
+      break;
 
-case 2:
-    /* code */
-    break;
-default:
-    break;
-}
-}
+    case 2:
+      /* code */
+      break;
+    default:
+      break;
+    }
+  }
 }
 
 #endif
